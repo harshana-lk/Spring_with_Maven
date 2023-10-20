1ba272093be6a56fd6c7dd94dbb014c200d4db9d
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-20 19:43:44 +0530

    Introduced Consumes , Produces & Headers

--- src/main/java/me/harshu/controller/CustomerController.java
+++ src/main/java/me/harshu/controller/CustomerController.java
@@ -8,13 +8,24 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("customer")
 public class CustomerController {
 
-    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public CustomerDTO getAllCustomers(){
         return new CustomerDTO("C001","Harshana","Badulla",15000.00);
+    }
+
+    @GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO searchCustomer(){
+        return new CustomerDTO("C002","Harshana","Badulla",15000.00);
     }
 
     @PostMapping
     public String saveCustomer(@RequestBody CustomerDTO customerDTO){
         return "Json Data : "+customerDTO;
     }
+
+    @GetMapping(headers = {"Content-Type=text/css","Accept=application/json"})
+    public CustomerDTO deleteCustomer(){
+        return new CustomerDTO("C003","Harshana","Badulla",15000.00);
+    }
+
 }
